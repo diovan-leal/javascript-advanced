942420651ed80e6beaef5b4576a793f6b2a34f8b
Diovan Leal <diovandol@gmail.com>
2025-04-12 08:12:20 -0300

    feat: adding construc and validation

--- mixing-data-with-funcionality/demo/triangle-oo.js
+++ mixing-data-with-funcionality/demo/triangle-oo.js
@@ -29,3 +29,61 @@ ta.getHypotenuse()
 ta instanceof Triangle
 typeof ta
 */
+
+/**Class with constructor */
+class TriangleWithConstructor {
+    constructor(sideA, sideB) {
+        this.a = sideA
+        this.b = sideB
+    }
+
+    getArea() {
+        return (this.a * this.b) / 2
+    }
+
+    getHypotenuse() {
+        return Math.sqrt(this.a ** 2 + this.b ** 2)
+    }
+
+    sayHi() {
+        return `Hello my name is ${this.constructor.name}`
+    }
+}
+
+/**Class whit validation in constructor */
+class TriangleWithConstructorValidation {
+    constructor(sideA, sideB) {
+        if (!Number.isFinite(sideA) || sideA <= 0) {
+            throw new Error(`Invalid number: ${sideA}`)
+        }
+
+        if (!Number.isFinite(sideB) || sideB <= 0) {
+            throw new Error(`Invalid number: ${sideB}`)
+        }
+
+        this.a = sideA
+        this.b = sideB
+    }
+
+    getArea() {
+        return (this.a * this.b) / 2
+    }
+
+    getHypotenuse() {
+        return Math.sqrt(this.a ** 2 + this.b ** 2)
+    }
+
+    sayHi() {
+        return `Hello my name is ${this.constructor.name}`
+    }
+}
+
+/**
+ * explore this funcionality in console browser
+ * ex:
+ta = new TriangleWithConstructor(4, 4)
+ta.getArea()
+ta.getHypotenuse()
+ */
+
+
