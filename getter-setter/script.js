167b9355b78b197e9d5a90d71ce9bf35a5af84b4
Diovan Leal <diovandol@gmail.com>
2025-04-22 09:08:56 -0300

    feat: method set and get and new Set

--- getter-setter/script.js
+++ getter-setter/script.js
@@ -1,6 +1,9 @@
 class Circle {
-    constructor(radius) {
-        this._radius = radius
+    static allowedCollors = new Set(["red", "green", "blue"])
+
+    constructor(radius, color) {
+        this.setRadius(radius)
+        this.setColor(color)
     }
 
     get diameter() {
@@ -9,5 +12,33 @@ class Circle {
 
     get radius() {
         return this._radius
+    }
+
+    setRadius(radius) {
+        if (radius < 0) {
+            throw new Error('Radius cannot be negative!')
+        }
+    
+        this._radius = radius
+    }
+
+    set radius(value) {
+        this.setRadius(value)
+    }
+
+    get color() {
+        return this._color
+    }
+
+    setColor(color) {
+        if (!Circle.allowedCollors.has(color)) {
+            throw new Error("That color is not allowed")
+        }
+
+        this._color = color
+    }
+
+    set color(color) {
+        this.setColor(color)
     }
 }
